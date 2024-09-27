bd55ea5d09b6ed103842b39dd94a1d5fa7215a23
Mitchell McKee <mckee.m@northeastern.edu>
2024-09-26 22:20:30 -0400

    Removed some boiler plate code

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,25 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
+  // refer to https://scryfall.com/docs/api 
+
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+      <h1>off meta</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          hype count is at {count}
         </button>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          The goal of this project is to provide an easy way to 
+          find the least commonly used cards in tournament decks
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
+        Placeholder Front-end for React Site that will be hosted on S3
       </p>
     </>
   )
