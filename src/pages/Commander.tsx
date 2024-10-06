ca0e688a440041f18dcd0bc2a5cd22f137501083
Mitchell McKee <mckee.m@northeastern.edu>
2024-10-05 20:52:05 -0400

    Added Data

--- src/pages/Commander.tsx
+++ src/pages/Commander.tsx
@@ -3,11 +3,29 @@ import { Link, useParams } from 'react-router-dom';
 import Typography from '@mui/joy/Typography';
 import styles from './Commander.module.css'
 import DisplayCard from '../components/card.component';
-import mockData from '../mocks/Talion, the Kindly Lord.json';
+import { CardInfo } from '../types/types';
+import { useQuery } from '@tanstack/react-query';
 
 function Commander() {
   const params = useParams();
+  const url = '/' + params.commanderName + '.json';
+  console.log(url)
+  const { isPending, error, data } = useQuery({
+    queryKey: ['repoData'],
+    queryFn: () =>
+      fetch(url).then((res) =>
+        res.json(),
+      ),
+  })
+  
+  if (isPending) {
+      return <div>Loading...</div>;
+  }
 
+  if (error) {
+    return <div>Error: {error.message}</div>;
+  }
+    
   return (
     <>
       <div className={styles.commander}>
@@ -15,7 +33,7 @@ function Commander() {
           <Typography color="primary" level='h2' sx={{ paddingTop: '8px'}}>{params.commanderName}</Typography>
         </Link>
         <div className={styles.cardContainer}>
-            {mockData.map(displayCard => (
+            {data.map((displayCard: CardInfo) => (
                 <div className={styles.displayCard} key={displayCard.id}>
                   <DisplayCard {...displayCard}> </DisplayCard>
                 </div>
